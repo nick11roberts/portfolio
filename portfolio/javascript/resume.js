211dc8d75f73dfab557469992594dc31b736d0ea
nick11roberts <n3robert@ucsd.edu>
2016-09-21 00:26:25 -0700

    Began writing html resume.

---
+++ portfolio/javascript/resume.js
@@ -0,0 +1,8 @@
+// Only executed our code once the DOM is ready.
+window.onload = function() {
+
+   // For canvas polygon bg
+   fillCanvas(document.getElementById('background'),
+      '#E2E0DE', 13, 13, 4);
+
+}
